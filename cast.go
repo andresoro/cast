2719abe30792cb9758ba8249b95675f0bd6c8623
Andres Orozco <ao15@my.fsu.edu>
2019-04-05 14:37:38 -0400

    add more test cases

--- cast.go
+++ cast.go
@@ -12,40 +12,42 @@ type Relay interface {
 }
 
 type cast struct {
-	input  <-chan []byte
-	output []chan<- []byte
-	end    chan struct{}
-	add    chan chan []byte
-	mu     sync.Mutex
+	input   <-chan []byte
+	outputs []chan<- []byte
+	end     chan struct{}
+	add     chan chan []byte
+	running bool
+	mu      sync.Mutex
 }
 
 // New returns a broacast interface from an input channel
 func New(input <-chan []byte) Relay {
 	return &cast{
-		input:  input,
-		output: make([]chan<- []byte, 0),
-		end:    make(chan struct{}),
-		add:    make(chan chan []byte),
+		input:   input,
+		outputs: make([]chan<- []byte, 0),
+		end:     make(chan struct{}),
+		add:     make(chan chan []byte),
 	}
 }
 
 func (c *cast) Start() {
-
+	c.running = true
 	go func() {
 		for {
 			select {
 			// handle incoming data
 			case data := <-c.input:
-				for _, ch := range c.output {
+				for _, ch := range c.outputs {
 					ch <- data
 				}
 			case ch := <-c.add:
-				c.output = append(c.output, ch)
+				c.outputs = append(c.outputs, ch)
 			// handle end signal
 			case <-c.end:
-				for _, ch := range c.output {
+				for _, ch := range c.outputs {
 					close(ch)
 				}
+				c.running = false
 				return
 			}
 
@@ -55,7 +57,12 @@ func (c *cast) Start() {
 }
 
 func (c *cast) Add(ch chan []byte) {
-	c.add <- ch
+	if c.running {
+		c.add <- ch
+	} else {
+		c.outputs = append(c.outputs, ch)
+	}
+
 }
 
 func (c *cast) Close() {
--- cast_test.go
+++ cast_test.go
@@ -1,39 +1,64 @@
 package cast
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestCast(t *testing.T) {
+func TestOutputs(t *testing.T) {
 
 	// where our values come from
 	producer := make(chan []byte)
 
-	outputCh1 := make(chan []byte, 1)
-	outputCh2 := make(chan []byte, 1)
-	outputCh3 := make(chan []byte, 1)
-
-	bcast := New(producer)
-	bcast.Start()
-	bcast.Add(outputCh1)
-	bcast.Add(outputCh2)
-	bcast.Add(outputCh3)
-
-	val := []byte("Hello World")
-
-	producer <- val
-
-	msg1 := <-outputCh1
-	if string(msg1) != string(val) {
-		t.Error("Not receiving value on ch1")
+	// output channels
+	outputs := make([]chan []byte, 0)
+	for i := 0; i < 5; i++ {
+		outputs = append(outputs, make(chan []byte, 1))
 	}
 
-	msg2 := <-outputCh2
-	if string(msg2) != string(val) {
-		t.Error("Not recieving value on ch2")
+	// init relay, add channels
+	relay := New(producer)
+	for _, ch := range outputs {
+		relay.Add(ch)
 	}
 
-	msg3 := <-outputCh3
-	if string(msg3) != string(val) {
-		t.Error("Not recieving value on ch3")
+	relay.Start()
+
+	// value added to producer
+	producer <- []byte("Hello World")
+
+	for i, ch := range outputs {
+		val := <-ch
+		if string(val) != "Hello World" {
+			t.Errorf("Channel %d not recieving value", i)
+		}
+
+	}
+}
+
+func TestAdding(t *testing.T) {
+	// adding before or after calling Start should not change behavior
+
+	producer := make(chan []byte)
+
+	output1 := make(chan []byte, 1)
+	output2 := make(chan []byte, 1)
+
+	relay := New(producer)
+
+	relay.Add(output1)
+	relay.Start()
+	relay.Add(output2)
+
+	producer <- []byte("Testing")
+
+	val1 := <-output1
+	if string(val1) != "Testing" {
+		t.Error("Output channel added before start not recieving value")
+	}
+
+	val2 := <-output2
+	if string(val2) != "Testing" {
+		t.Error("Output channel after Start not recieving value")
 	}
 
 }
